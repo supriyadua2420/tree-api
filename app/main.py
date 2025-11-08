5fdaa6e89da4464081d9e9b18d6c4fa3faf331ef
Supriya Dua <supriya@hiringbranch.com>
2025-11-07 22:18:54 -0800

    add cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from app.routes import nodes
 from app.database import db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Include your nodes router
 app.include_router(nodes.router)
+
+origins = [
+    "http://localhost:5173", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
