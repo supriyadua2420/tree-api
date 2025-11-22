15cbcf33ad3e50aa9217c88982120e99d254ba30
Supriya Dua <supriya@hiringbranch.com>
2025-11-21 23:25:35 -0800

    coordinates

--- app/models.py
+++ app/models.py
@@ -5,3 +5,11 @@ class Node(BaseModel):
     id: Optional[str]
     label: str
     parent_id: Optional[str] = None
+    x: float = 0.0
+    y: float = 0.0
+
+class NodeUpdate(BaseModel):
+    label: Optional[str] = None
+    parent_id: Optional[str] = None
+    x: Optional[float] = None
+    y: Optional[float] = None
